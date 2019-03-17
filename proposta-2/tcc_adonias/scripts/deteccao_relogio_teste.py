57f854ed740d7760f12cdedb1b87712b0c584e0b
Adonias <adoniaslimagouveia@outlook.com>
2019-03-17 09:48:28 -0300

    primeiro teste com imagens de garrafas e etiquetas

--- proposta-2/tcc_adonias/scripts/deteccao_relogio_teste.py
+++ proposta-2/tcc_adonias/scripts/deteccao_relogio_teste.py
@@ -3,13 +3,13 @@ import dlib
 import cv2
 import glob
 
-detectorRelogio = dlib.simple_object_detector("../treinamento_xml/detector_relogios.svm")
-for imagem in glob.glob(os.path.join("../../relogios_teste2", "*.jpg")):
+detectorRelogio = dlib.simple_object_detector("../treinamento_xml/detector_etiquetas.svm")
+for imagem in glob.glob(os.path.join("../imagens_etiquetas", "*.jpg")):
     img = cv2.imread(imagem)
     objetos_detectados = detectorRelogio(img, 2)
     for d in objetos_detectados:
         e, t, d, b = (int(d.left()), int(d.top()), int(d.right()), int(d.bottom()))
         cv2.rectangle(img, (e, t), (d, b), (0, 0, 255), 2)
-    cv2.imshow("Detector de relogios", img)
+    cv2.imshow("Detector", img)
     cv2.waitKey(0)
 cv2.destroyAllWindows()
