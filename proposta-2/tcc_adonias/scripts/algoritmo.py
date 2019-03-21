9e2871cdf6e0baa12e8348d90855e95e6b4ea40a
Adonias <adoniaslimagouveia@outlook.com>
2019-03-21 19:53:20 -0300

    adicioando print ao retangulo

--- proposta-2/tcc_adonias/scripts/algoritmo.py
+++ proposta-2/tcc_adonias/scripts/algoritmo.py
@@ -17,15 +17,21 @@ import glob
 # Data 18/03/2019
 # Descrição: Teste para implementação do algoritmo, criação da função executarApp()
 
-dados = dlib.test_simple_object_detector("../treinamento_xml/treinamento_etiquetas.xml", "../treinamento_xml/detector_etiquetas.svm")
+dados = dlib.test_simple_object_detector("../treinamento_xml/treinamento_garrafas.xml", "../treinamento_xml/detector_garrafas.svm")
 print(dados.average_precision) #dados é uma variável que está recebendo a classe acima.
 
-detectorRelogio = dlib.simple_object_detector("../treinamento_xml/detector_etiquetas.svm")
-for imagem in glob.glob(os.path.join("../imagens_etiquetas", "etiqueta2.jpg")):
+if dados.average_precision > 0.5:
+        print("É um valor maior!")
+else:
+        print('É um valor menor!')
+
+detectorRelogio = dlib.simple_object_detector("../treinamento_xml/detector_garrafas.svm")
+for imagem in glob.glob(os.path.join("../imagens_garrafas", "*.jpg")):
     img = cv2.imread(imagem)
     objetos_detectados = detectorRelogio(img, 2)
     for d in objetos_detectados:
         e, t, d, b = (int(d.left()), int(d.top()), int(d.right()), int(d.bottom()))
+        print(e,t,d,b)
         cv2.rectangle(img, (e, t), (d, b), (0, 0, 255), 2)
     cv2.imshow("Detector", img)
     cv2.waitKey(0)
--- proposta-2/tcc_adonias/scripts/deteccao_relogios_treinamento.py
+++ proposta-2/tcc_adonias/scripts/deteccao_relogios_treinamento.py
@@ -4,4 +4,4 @@ opcoes = dlib.simple_object_detector_training_options()
 opcoes.add_left_right_image_flips = True
 opcoes.C = 5
 
-dlib.train_simple_object_detector("../treinamento_xml/treinamento_relogios_adonias.xml", "../treinamento_xml/detector_relogios.svm", opcoes)
+dlib.train_simple_object_detector("../treinamento_xml/garrafas_new.xml", "../treinamento_xml/garrafas_new.svm", opcoes)
